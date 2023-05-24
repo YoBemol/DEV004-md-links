e5f0947f2559bc19b9258453fda19a45dc339548
Belen Molina <belen.molina.z@gmail.com>
2023-05-24 09:54:16 -0500

    peticion HTTP con axios

--- cli.js
+++ cli.js
@@ -3,16 +3,10 @@ import { mdLinks } from "./index.js";
 // const [, , argument] = process.argv;
 // console.log(argument);
 const ruta = process.argv[2];
-if(process.argv.includes('--validate') === false){
-    mdLinks(ruta, {validate:false})
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((error) => {
-        console.log(error)
-    });
-}else if(process.argv.includes('--validate') === true || process.argv.includes('-v') ){
-    mdLinks(ruta, {validate:true})
+const processArr = process.argv;
+if(processArr[3] === '--v' || processArr[3] === '--validate' ){
+    // console.log(true);
+        mdLinks(ruta, {validate:true})
     .then((data) => {
         console.log(data);
     })
@@ -20,4 +14,41 @@ if(process.argv.includes('--validate') === false){
         console.log(error)
     });
 }
+
+if(processArr.length === 3){
+    // console.log('no validate');
+        mdLinks(ruta, {validate:false})
+    .then((data) => {
+        console.log(data);
+        // data.forEach(element => {
+        //     console.log(element.text);
+        // });
+    })
+    .catch((error) => {
+        console.log(error)
+    });
+}
+
+// const prueba = process.argv
+// console.log(prueba.includes('--v') === '--v', 'process');
+// if(process.argv[3] === 'validate' || process.argv[3] === '-v' ){
+//     mdLinks(ruta, {validate:false})
+//     .then((data) => {
+//         console.log('hola1',data);
+//         // data.forEach(element => {
+//         //     console.log(element.text);
+//         // });
+//     })
+//     .catch((error) => {
+//         console.log(error)
+//     });
+// }// else if(process.argv[3]idate') === true || process.argv[3] === true ){
+//     mdLinks(ruta, {validate:true})
+//     .then((data) => {
+//         console.log('hola2',data);
+//     })
+//     .catch((error) => {
+//         console.log(error)
+//     });
+// }
 //if(pro)
--- ejemplo.md
+++ ejemplo.md
@@ -11,3 +11,6 @@
   * [Generalidades del protocolo HTTP - MDN](https://developer.mozilla.org/es/docs/Web/HTTP/Overview)
   * [Mensajes HTTP - MDN](https://developer.mozilla.org/es/docs/Web/HTTP/Messages)
 </p></details>
+
+
+* [Módulos, librerías, paquetes, frameworks... ¿cuál es la diferencia?](http://community.laboratoria.la/t/modulos-librerias-paquetes-frameworks-cual-es-la-diferencia/175)
